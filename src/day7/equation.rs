b10e2746c02765495d218a02dc43b6a48a6f702c
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-07 13:40:50 +0000

    Day 7 Part 1 find possible combinations

--- src/day7/equation.rs
+++ src/day7/equation.rs
@@ -13,6 +13,15 @@ enum Operator {
     Multiply,
 }
 
+impl Operator {
+    fn apply(&self, left: usize, right: usize) -> usize {
+        match self {
+            Add => left + right,
+            Multiply => left * right,
+        }
+    }
+}
+
 impl Equation {
     pub fn parse_to_vec(string: &str) -> Vec<Equation> {
         string.lines().map(Equation::parse).collect()
@@ -30,6 +39,10 @@ impl Equation {
             .unwrap();
         Equation { numbers, equals }
     }
+    pub fn possible_operator_combinations(&self) -> impl Iterator<Item = Vec<Operator>> + use<'_> {
+        self.all_operator_combinations()
+            .filter(|c| self.is_combination_possible(c))
+    }
     fn all_operator_combinations(&self) -> impl Iterator<Item = Vec<Operator>> {
         let num_operators = self.numbers.len() - 1;
         let num_combinations = 2_usize.pow(num_operators as u32);
@@ -41,6 +54,15 @@ impl Equation {
                 })
                 .collect::<Vec<Operator>>()
         })
+    }
+    fn is_combination_possible(&self, operators: &Vec<Operator>) -> bool {
+        let mut acc = self.numbers[0];
+        for i in 0..(self.numbers.len() - 1) {
+            let right = self.numbers[i + 1];
+            let operator = operators[i];
+            acc = operator.apply(acc, right);
+        }
+        acc == self.equals
     }
 }
 
@@ -96,6 +118,16 @@ mod tests {
             print_all_combinations(&equation),
             vec!["+++", "*++", "+*+", "**+", "++*", "*+*", "+**", "***"]
         );
+    }
+
+    #[test]
+    fn can_find_possible_combinations_for_three_operators() {
+        let equation = Equation::parse("8: 2 2 2 2");
+        assert_eq!(print_possible_combinations(&equation), vec!["+++", "*++"]);
+    }
+
+    fn print_possible_combinations(equation: &Equation) -> Vec<String> {
+        print_combinations(equation.possible_operator_combinations().collect())
     }
 
     fn print_all_combinations(equation: &Equation) -> Vec<String> {
