eece6d7b1226fffdea34182b83541cdf135beb89
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-08 11:49:24 +0000

    Day 8 Part 1 read width/height

--- src/day8/antenna_map.rs
+++ src/day8/antenna_map.rs
@@ -1,10 +1,44 @@
-pub struct AntennaMap {}
+pub struct AntennaMap {
+    width: usize,
+    height: usize,
+}
 
 impl AntennaMap {
-    pub fn parse(_: &str) -> AntennaMap {
-        AntennaMap {}
+    pub fn parse(string: &str) -> AntennaMap {
+        let width = string.lines().next().unwrap().len();
+        let height = string.lines().count();
+        AntennaMap { width, height }
     }
     pub fn count_unique_antinode_locations(&self) -> usize {
         0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day8::antenna_map::AntennaMap;
+
+    #[test]
+    fn can_plot_map() {
+        let input = "\
+                ...\n\
+                ...\n";
+        assert_eq!(
+            plot_map(&AntennaMap::parse(input)),
+            "...\n\
+             ...\n"
+                .to_string()
+        );
+    }
+
+    fn plot_map(map: &AntennaMap) -> String {
+        let mut str = "".to_string();
+        for _ in 0..map.height {
+            for _ in 0..map.width {
+                str.push('.');
+            }
+            str.push('\n');
+        }
+        str
     }
 }
