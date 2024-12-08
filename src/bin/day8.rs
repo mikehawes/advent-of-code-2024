ac33bc2bbc06e7a13f65fb332ca1bb7ba57aba7e
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-08 22:15:56 +0000

    Day 8 Part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -5,4 +5,5 @@ fn main() {
     let input = input_to_string("day8/input.txt").unwrap();
     let map = AntennaMap::parse(input.as_str());
     println!("Part 1: {}", map.count_unique_antinode_locations());
+    println!("Part 2: {}", map.count_unique_extended_antinode_locations());
 }
--- src/day8/antenna_map.rs
+++ src/day8/antenna_map.rs
@@ -39,10 +39,18 @@ impl AntennaMap {
         let antinodes: HashSet<Point> = self.iter_antinodes().collect();
         antinodes.len()
     }
+    pub fn count_unique_extended_antinode_locations(&self) -> usize {
+        let antinodes: HashSet<Point> = self.iter_extended_antinodes().collect();
+        antinodes.len()
+    }
     fn iter_antinodes(&self) -> impl Iterator<Item = Point> + use<'_> {
         self.all_antenna_combinations()
             .flat_map(|(left, right)| antinodes_of_points(left, right))
             .filter(|point| self.is_in_grid(*point))
+    }
+    fn iter_extended_antinodes(&self) -> impl Iterator<Item = Point> + use<'_> {
+        self.all_antenna_combinations()
+            .flat_map(|(left, right)| self.extended_antinodes_of_points(left, right))
     }
     fn all_antenna_combinations(&self) -> impl Iterator<Item = (Point, Point)> + use<'_> {
         self.positions_by_frequency
@@ -52,6 +60,30 @@ impl AntennaMap {
     fn is_in_grid(&self, point: Point) -> bool {
         let (x, y) = point;
         x < self.width && y < self.height
+    }
+    fn extended_antinodes_of_points(&self, left: Point, right: Point) -> Vec<Point> {
+        let (x1, y1) = to_vector(left);
+        let (x2, y2) = to_vector(right);
+        let xdiff = x2 - x1;
+        let ydiff = y2 - y1;
+        let mut antinodes = vec![left, right];
+        self.extend_antinodes((x1, y1), (-xdiff, -ydiff), &mut antinodes);
+        self.extend_antinodes((x2, y2), (xdiff, ydiff), &mut antinodes);
+        antinodes
+    }
+    fn extend_antinodes(&self, origin: Vector, diff: Vector, antinodes: &mut Vec<Point>) {
+        let (mut x, mut y) = origin;
+        let (dx, dy) = diff;
+        loop {
+            x += dx;
+            y += dy;
+            let point = to_point((x, y));
+            if self.is_in_grid(point) {
+                antinodes.push(point);
+            } else {
+                return;
+            }
+        }
     }
 }
 
@@ -94,7 +126,13 @@ mod tests {
     #[test]
     fn can_plot_example() {
         let input = input_to_string("day8/example.txt").unwrap();
-        assert_snapshot!(plot_map(&AntennaMap::parse(input.as_str())));
+        assert_snapshot!(plot_map(input.as_str()));
+    }
+
+    #[test]
+    fn can_plot_example_extended() {
+        let input = input_to_string("day8/example.txt").unwrap();
+        assert_snapshot!(plot_map_extended(input.as_str()));
     }
 
     #[test]
@@ -105,6 +143,13 @@ mod tests {
     }
 
     #[test]
+    fn can_count_extendedantinodes_for_example() {
+        let input = input_to_string("day8/example.txt").unwrap();
+        let map = AntennaMap::parse(input.as_str());
+        assert_eq!(map.count_unique_extended_antinode_locations(), 34)
+    }
+
+    #[test]
     fn can_find_antenna_combinations() {
         assert_eq!(
             all_antenna_combinations_by_number("aaa"),
@@ -112,11 +157,23 @@ mod tests {
         );
     }
 
-    fn plot_map(map: &AntennaMap) -> String {
-        let mut point_to_char = index_frequency_by_position(map);
+    fn plot_map(map_string: &str) -> String {
+        let map = AntennaMap::parse(map_string);
+        let mut point_to_char = index_frequency_by_position(&map);
         map.iter_antinodes().for_each(|point| {
             point_to_char.entry(point).or_insert('#');
         });
+        plot_map_with_chars(&map, point_to_char)
+    }
+    fn plot_map_extended(map_string: &str) -> String {
+        let map = AntennaMap::parse(map_string);
+        let mut point_to_char = index_frequency_by_position(&map);
+        map.iter_extended_antinodes().for_each(|point| {
+            point_to_char.entry(point).or_insert('#');
+        });
+        plot_map_with_chars(&map, point_to_char)
+    }
+    fn plot_map_with_chars(map: &AntennaMap, point_to_char: HashMap<Point, char>) -> String {
         let mut str = "".to_string();
         for y in 0..map.height {
             for x in 0..map.width {
