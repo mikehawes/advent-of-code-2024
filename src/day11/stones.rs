36ed6c77f6e198a9529e6e691a5c78a85cb49820
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-11 16:35:05 +0000

    Day 11 Part 1 parse stones

--- src/day11/stones.rs
+++ src/day11/stones.rs
@@ -1,13 +1,41 @@
-pub struct Stones {}
+use std::str::FromStr;
+
+pub struct Stones {
+    stones: Vec<usize>,
+}
 
 impl Stones {
-    pub fn parse(_: &str) -> Stones {
-        Stones {}
+    pub fn parse(string: &str) -> Stones {
+        let stones = string
+            .split_whitespace()
+            .map(|stone| usize::from_str(stone).unwrap())
+            .collect();
+        Stones { stones }
     }
     pub fn blink_times(&mut self, _: usize) -> &Stones {
         self
     }
     pub fn count_stones(&self) -> usize {
         0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day11::stones::Stones;
+
+    #[test]
+    fn can_parse_stones() {
+        let stones = Stones::parse("0 1 10 99 999");
+        assert_eq!(print(&stones), "0 1 10 99 999")
+    }
+
+    fn print(stones: &Stones) -> String {
+        stones
+            .stones
+            .iter()
+            .map(|stone| stone.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
     }
 }
