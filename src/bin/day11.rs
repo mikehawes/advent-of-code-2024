cece36130b62724c2c147e3c580459c24e8dd4bd
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-11 18:25:50 +0000

    Day 11 Part 2 explore sequence

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -4,5 +4,6 @@ use advent_of_code_2024::input::input_to_string;
 fn main() {
     let input = input_to_string("day11/input.txt").unwrap();
     let stones = Stones::parse(input.as_str());
-    println!("Part 1: {}", stones.blink_times(25).count_stones());
+    println!("Part 1: {}", stones.count_stones_after_blinks(25));
+    println!("Part 2: {}", stones.count_stones_after_blinks(75));
 }
--- src/day11/stones.rs
+++ src/day11/stones.rs
@@ -14,14 +14,17 @@ impl Stones {
             .collect();
         Stones { stones }
     }
-    pub fn blink_times(&self, times: usize) -> Stones {
+    pub fn count_stones_after_blinks(&self, times: usize) -> usize {
+        self.blink_times(times).count_stones()
+    }
+    fn blink_times(&self, times: usize) -> Stones {
         let mut stones = self.clone();
         for _ in 0..times {
             stones = stones.blink();
         }
         stones
     }
-    pub fn count_stones(&self) -> usize {
+    fn count_stones(&self) -> usize {
         self.stones.len()
     }
     fn blink(&self) -> Stones {
@@ -83,9 +86,81 @@ mod tests {
     }
 
     #[test]
-    fn can_count_stones_after_25_blinks() {
+    fn can_count_stones_after_25_blinks_second_example() {
         let stones = Stones::parse("125 17");
         assert_eq!(stones.blink_times(25).count_stones(), 55312)
+    }
+
+    #[test]
+    fn can_find_number_of_stones_sequence() {
+        let stones = Stones::parse("125 17");
+        let sequence: Vec<usize> = (0..=25)
+            .map(|blinks| stones.blink_times(blinks))
+            .map(|stones| stones.count_stones())
+            .collect();
+        assert_eq!(
+            sequence,
+            vec![
+                2, 3, 4, 5, 9, 13, 22, 31, 42, 68, 109, 170, 235, 342, 557, 853, 1298, 1951, 2869,
+                4490, 6837, 10362, 15754, 23435, 36359, 55312
+            ]
+        )
+    }
+
+    #[test]
+    fn can_find_number_of_stones_sequence_for_one_stone() {
+        let stones = Stones::parse("125");
+        let sequence: Vec<usize> = (0..=30)
+            .map(|blinks| stones.blink_times(blinks))
+            .map(|stones| stones.count_stones())
+            .collect();
+        assert_eq!(
+            sequence,
+            vec![
+                1, 1, 2, 2, 3, 5, 7, 9, 15, 26, 38, 56, 82, 126, 189, 283, 467, 682, 978, 1575,
+                2395, 3625, 5390, 8173, 12846, 19025, 28994, 44502, 66490, 102144, 154793
+            ]
+        )
+    }
+
+    #[test]
+    fn can_find_number_of_stones_sequence_for_zero() {
+        let stones = Stones::parse("0");
+        let sequence: Vec<usize> = (0..=30)
+            .map(|blinks| stones.blink_times(blinks))
+            .map(|stones| stones.count_stones())
+            .collect();
+        assert_eq!(
+            sequence,
+            vec![
+                1, 1, 1, 2, 4, 4, 7, 14, 16, 20, 39, 62, 81, 110, 200, 328, 418, 667, 1059, 1546,
+                2377, 3572, 5602, 8268, 12343, 19778, 29165, 43726, 67724, 102131, 156451
+            ]
+        )
+    }
+
+    #[test]
+    fn can_blink_zero() {
+        let stones = Stones::parse("0");
+        let blinks: Vec<String> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            .iter()
+            .map(|blinks| stones.blink_times(*blinks))
+            .map(|stones| print(&stones))
+            .collect();
+        assert_eq!(
+            blinks,
+            vec!["0",
+                 "1",
+                 "2024",
+                 "20 24",
+                 "2 0 2 4",
+                 "4048 1 4048 8096",
+                 "40 48 2024 40 48 80 96",
+                 "4 0 4 8 20 24 4 0 4 8 8 0 9 6",
+                 "8096 1 8096 16192 2 0 2 4 8096 1 8096 16192 16192 1 18216 12144",
+                 "80 96 2024 80 96 32772608 4048 1 4048 8096 80 96 2024 80 96 32772608 32772608 2024 36869184 24579456",
+                 "8 0 9 6 20 24 8 0 9 6 3277 2608 40 48 2024 40 48 80 96 8 0 9 6 20 24 8 0 9 6 3277 2608 3277 2608 20 24 3686 9184 2457 9456"]
+        )
     }
 
     fn print(stones: &Stones) -> String {
