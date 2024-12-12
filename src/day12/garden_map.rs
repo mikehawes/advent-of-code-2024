6d40e1cf866f00d1af4a134b3d9c7849c00fb101
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-12 14:37:06 +0000

    Day 12 Part 1 number regions

--- src/day12/garden_map.rs
+++ src/day12/garden_map.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub struct GardenMap {
     plots: Vec<Vec<char>>,
     width: usize,
@@ -25,11 +27,25 @@ impl GardenMap {
     pub fn sum_fencing_price(&self) -> usize {
         0
     }
+    fn plant_at(&self, point: Point) -> char {
+        let (x, y) = point;
+        self.plots[y][x]
+    }
+    fn adjacent_points(&self, point: Point) -> Vec<Point> {
+        adjacent_points(point)
+            .iter()
+            .filter(|p| self.is_on_map(**p))
+            .copied()
+            .collect()
+    }
+    fn is_on_map(&self, point: Point) -> bool {
+        let (x, y) = point;
+        x < self.width && y < self.height
+    }
 }
 
 struct RegionIndex<'a> {
     map: &'a GardenMap,
-    regions: Vec<Region>,
     plot_region_numbers: Vec<Vec<usize>>,
 }
 
@@ -37,23 +53,75 @@ type Point = (usize, usize);
 
 impl<'a> RegionIndex<'a> {
     fn from(map: &GardenMap) -> RegionIndex {
-        let regions = Vec::new();
-        let plot_region_numbers =
-            Vec::from_iter((0..map.height).map(|_| Vec::from_iter(0..map.width)));
+        let mut point_to_region_number: HashMap<Point, usize> = HashMap::new();
+        let mut region_number = 0;
+        for y in 0..map.height {
+            for x in 0..map.width {
+                let point = (x, y);
+                if !point_to_region_number.contains_key(&point) {
+                    map_region_from(point, region_number, map, &mut point_to_region_number);
+                    region_number += 1;
+                }
+            }
+        }
+        let plot_region_numbers = Vec::from_iter(
+            (0..map.height)
+                .map(|y| Vec::from_iter((0..map.width).map(|x| point_to_region_number[&(x, y)]))),
+        );
         RegionIndex {
             map,
-            regions,
             plot_region_numbers,
         }
     }
-    fn region_number_at(&self, _: Point) -> usize {
-        0
+    fn region_number_at(&self, point: Point) -> usize {
+        let (x, y) = point;
+        self.plot_region_numbers[y][x]
     }
 }
 
-struct Region {
-    number: usize,
+fn map_region_from(
+    start_point: Point,
+    region_number: usize,
+    map: &GardenMap,
+    point_to_region_number: &mut HashMap<Point, usize>,
+) {
+    let plant = map.plant_at(start_point);
+    map_region(
+        start_point,
+        region_number,
+        plant,
+        map,
+        point_to_region_number,
+    )
+}
+
+fn map_region(
+    point: Point,
+    region_number: usize,
     plant: char,
+    map: &GardenMap,
+    point_to_region_number: &mut HashMap<Point, usize>,
+) {
+    point_to_region_number.insert(point, region_number);
+    for adjacent in map.adjacent_points(point) {
+        if point_to_region_number.contains_key(&adjacent) {
+            continue;
+        }
+        let adjacent_plant = map.plant_at(adjacent);
+        if plant == adjacent_plant {
+            map_region(adjacent, region_number, plant, map, point_to_region_number);
+        }
+    }
+}
+
+fn adjacent_points(point: Point) -> Vec<Point> {
+    let (x, y) = point;
+    vec![
+        (x + 1, y),
+        (x, y + 1),
+        (x.wrapping_sub(1), y),
+        (x, y.wrapping_sub(1)),
+    ]
 }
 
 #[cfg(test)]
@@ -82,9 +150,9 @@ mod tests {
         let region_index = RegionIndex::from(&map);
         let expected = "\
             0000\n\
-            0000\n\
-            0000\n\
-            0000\n";
+            1123\n\
+            1122\n\
+            4442\n";
         assert_eq!(print_region_numbers(&region_index), expected)
     }
 
