fa9ff4e68cbc415ecd97a4788e0e0a1aa45644d6
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-12 12:34:41 +0000

    Day 12 Part 1 setup region index

--- src/day12/garden_map.rs
+++ src/day12/garden_map.rs
@@ -27,9 +27,38 @@ impl GardenMap {
     }
 }
 
+struct RegionIndex<'a> {
+    map: &'a GardenMap,
+    regions: Vec<Region>,
+    plot_region_numbers: Vec<Vec<usize>>,
+}
+
+type Point = (usize, usize);
+
+impl<'a> RegionIndex<'a> {
+    fn from(map: &GardenMap) -> RegionIndex {
+        let regions = Vec::new();
+        let plot_region_numbers =
+            Vec::from_iter((0..map.height).map(|_| Vec::from_iter(0..map.width)));
+        RegionIndex {
+            map,
+            regions,
+            plot_region_numbers,
+        }
+    }
+    fn region_number_at(&self, _: Point) -> usize {
+        0
+    }
+}
+
+struct Region {
+    number: usize,
+    plant: char,
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day12::garden_map::GardenMap;
+    use crate::day12::garden_map::{GardenMap, RegionIndex};
 
     #[test]
     fn can_parse_map() {
@@ -42,6 +71,23 @@ mod tests {
         assert_eq!(print(&map), string)
     }
 
+    #[test]
+    fn can_number_regions() {
+        let string = "\
+            AAAA\n\
+            BBCD\n\
+            BBCC\n\
+            EEEC\n";
+        let map = GardenMap::parse(string);
+        let region_index = RegionIndex::from(&map);
+        let expected = "\
+            0000\n\
+            0000\n\
+            0000\n\
+            0000\n";
+        assert_eq!(print_region_numbers(&region_index), expected)
+    }
+
     fn print(map: &GardenMap) -> String {
         let mut str = String::new();
         for line in map.plots.iter() {
@@ -52,5 +98,17 @@ mod tests {
     }
     fn print_line(line: &[char]) -> String {
         line.iter().collect()
+    }
+
+    fn print_region_numbers(region_index: &RegionIndex) -> String {
+        let mut str = String::new();
+        for y in 0..region_index.map.height {
+            for x in 0..region_index.map.width {
+                let region_number = region_index.region_number_at((x, y));
+                str.push_str(&region_number.to_string());
+            }
+            str.push('\n');
+        }
+        str
     }
 }
