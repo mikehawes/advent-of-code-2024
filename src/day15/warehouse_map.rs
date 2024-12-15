9e381cde1a90ebef541ebdfaa82430a084889db7
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-15 10:25:50 +0000

    Day 15 Part 1 clean up map

--- src/day15/warehouse_map.rs
+++ src/day15/warehouse_map.rs
@@ -13,10 +13,11 @@ pub struct WarehouseMap {
 
 impl WarehouseMap {
     pub fn parse_map_and_robot_position(string: &str) -> (WarehouseMap, Point) {
-        let tiles: Vec<Vec<char>> = string.lines().map(|line| line.chars().collect()).collect();
-        let width = tiles.first().map(|line| line.len()).unwrap_or(0);
-        let height = tiles.len();
-        let robot_position = find_robot(&tiles, width, height);
+        let map: Vec<Vec<char>> = string.lines().map(|line| line.chars().collect()).collect();
+        let width = map.first().map(|line| line.len()).unwrap_or(0);
+        let height = map.len();
+        let robot_position = find_robot(&map, width, height);
+        let tiles = map_walls_and_boxes(&map);
         (
             WarehouseMap {
                 tiles,
@@ -28,12 +29,7 @@ impl WarehouseMap {
     }
     fn tile(&self, point: Point) -> char {
         let [x, y] = point;
-        let tile = self.tiles[y][x];
-        match tile {
-            WALL => WALL,
-            BOX => BOX,
-            _ => EMPTY,
-        }
+        self.tiles[y][x]
     }
 }
 
@@ -42,6 +38,21 @@ fn find_robot(tiles: &[Vec<char>], width: usize, height: usize) -> Point {
         .flat_map(|y| (0..width).map(move |x| [x, y]))
         .find(|[x, y]| tiles[*y][*x] == ROBOT)
         .unwrap_or([width, height])
+}
+
+fn map_walls_and_boxes(tiles: &[Vec<char>]) -> Vec<Vec<char>> {
+    tiles
+        .iter()
+        .map(|line| {
+            line.iter()
+                .map(|tile| match *tile {
+                    WALL => WALL,
+                    BOX => BOX,
+                    _ => EMPTY,
+                })
+                .collect()
+        })
+        .collect()
 }
 
 #[cfg(test)]
