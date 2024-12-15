958d432d2346f1c740fdb51be2fa346af337338e
mikehawes <152744602+mikehawes@users.noreply.github.com>
2024-12-15 10:15:37 +0000

    Day 15 Part 1 find robot

--- src/day15/warehouse_map.rs
+++ src/day15/warehouse_map.rs
@@ -1,7 +1,15 @@
+const WALL: char = '#';
+const BOX: char = 'O';
+const EMPTY: char = '.';
+const ROBOT: char = '@';
+
+type Point = [usize; 2];
+
 pub struct WarehouseMap {
     tiles: Vec<Vec<char>>,
     width: usize,
     height: usize,
+    robot_position: Point,
 }
 
 impl WarehouseMap {
@@ -9,12 +17,21 @@ impl WarehouseMap {
         let tiles: Vec<Vec<char>> = string.lines().map(|line| line.chars().collect()).collect();
         let width = tiles.first().map(|line| line.len()).unwrap_or(0);
         let height = tiles.len();
+        let robot_position = find_robot(&tiles, width, height);
         WarehouseMap {
             tiles,
             width,
             height,
+            robot_position,
         }
     }
+}
+
+fn find_robot(tiles: &[Vec<char>], width: usize, height: usize) -> Point {
+    (0..height)
+        .flat_map(|y| (0..width).map(move |x| [x, y]))
+        .find(|[x, y]| tiles[*y][*x] == ROBOT)
+        .unwrap_or([width, height])
 }
 
 #[cfg(test)]
@@ -33,7 +50,8 @@ mod tests {
             #......#\n\
             ########\n";
         let map = WarehouseMap::parse(string);
-        assert_eq!(print(&map), string)
+        assert_eq!(print(&map), string);
+        assert_eq!(map.robot_position, [2, 2]);
     }
 
     fn print(map: &WarehouseMap) -> String {
